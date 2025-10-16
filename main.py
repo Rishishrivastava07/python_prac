3e84e17c623d062148e3418d147e4407e79b84b4
Rishishrivastava07 <rishishrivastava.91@gmail.com>
2025-10-16 23:46:54 +0530

    checkpoint

--- main.py
+++ main.py
@@ -7,7 +7,19 @@
 
 # function to add two numbers:
 
-def add(x, y):
-    return x + y
+# def add(x, y):
+#     return x + y
 
-print(add(4, 5))
+# print(add(4, 5))
+
+# function to find the largest number among three numbers:
+
+# def largest(a, b, c):
+#     if (a >= b) and (a >= c):
+#         return a
+#     elif (b >= a) and (b >= c):
+#         return b
+#     else:
+#         return c
+    
+# print(largest(4, 5, 2))
