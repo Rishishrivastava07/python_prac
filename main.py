bcc06037957dc1d7f8bfc3af819a120c3161a95c
Rishishrivastava07 <rishishrivastava.91@gmail.com>
2025-10-16 23:11:26 +0530

    checkpoint

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-a = int(input("enter a number: "))
+# prints table
+# a = int(input("enter a number: "))
 
-for i in range (a, (10*a)+1, a):
-    print(i)    
+# for i in range (a, (10*a)+1, a):
+#     print(i)    
+
+
+# function to add two numbers:
+
+def add(x, y):
+    return x + y
+
+print(add(4, 5))
